de82ec9f93f1327a07259b3e3af8dc2aa8e849f1
moi <moi@moi.com>
2017-09-07 16:10:23 +0200

    ajout conditions victoire

--- app/src/main/java/com/cdi/mymorpion/MainActivity.java
+++ app/src/main/java/com/cdi/mymorpion/MainActivity.java
@@ -8,20 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     boolean toggle = true;
+    String isWin;
 
     TextView tv;
-    Button b1;
-    Button b2;
-    Button b3;
-    Button b4;
-    Button b5;
-    Button b6;
-    Button b7;
-    Button b8;
-    Button b9;
+    Button b1, b2, b3, b4, b5, b6, b7, b8, b9;
 
     ConstraintLayout cl;
 
@@ -44,202 +39,95 @@ public class MainActivity extends AppCompatActivity {
         b8 = (Button) findViewById(R.id.button8);
         b9 = (Button) findViewById(R.id.button9);
 
-        tv.setText("Joueur O, à vous de jouer !");
+
         cl.setBackgroundColor(Color.rgb(118, 152, 231));
+        tv.setText("Joueur O, à vous de jouer !");
 
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (b1.getText().toString().isEmpty()) {
-                    if (toggle) {
-                        b1.setText("O");
-                        toggle = false;
-                        tv.setText("Joueur X, à vous de jouer !");
-                        cl.setBackgroundColor(Color.rgb(224, 134, 124));
-                    } else {
-                        b1.setText("X");
-                        toggle = true;
-                        tv.setText("Joueur O, à vous de jouer !");
-                        cl.setBackgroundColor(Color.rgb(118, 152, 231));
-                    }
-                }
-            }
-        });
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (b2.getText().toString().isEmpty()) {
-                    if (toggle) {
-                        b2.setText("O");
-                        toggle = false;
-                        tv.setText("Joueur X, à vous de jouer !");
-                        cl.setBackgroundColor(Color.rgb(224, 134, 124));
-                    } else {
-                        b2.setText("X");
-                        toggle = true;
-                        tv.setText("Joueur O, à vous de jouer !");
-                        cl.setBackgroundColor(Color.rgb(118, 152, 231));
-                    }
-                }
-            }
-        });
-        b3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (b3.getText().toString().isEmpty()) {
-                    if (toggle) {
-                        b3.setText("O");
-                        toggle = false;
-                        tv.setText("Joueur X, à vous de jouer !");
-                        cl.setBackgroundColor(Color.rgb(224, 134, 124));
-                    } else {
-                        b3.setText("X");
-                        toggle = true;
-                        tv.setText("Joueur O, à vous de jouer !");
-                        cl.setBackgroundColor(Color.rgb(118, 152, 231));
-                    }
-                }
-            }
-        });
-        b4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (b4.getText().toString().isEmpty()) {
-                    if (toggle) {
-                        b4.setText("O");
-                        toggle = false;
-                        tv.setText("Joueur X, à vous de jouer !");
-                        cl.setBackgroundColor(Color.rgb(224, 134, 124));
-                    } else {
-                        b4.setText("X");
-                        toggle = true;
-                        tv.setText("Joueur O, à vous de jouer !");
-                        cl.setBackgroundColor(Color.rgb(118, 152, 231));
-                    }
-                }
-            }
-        });
-        b5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (b5.getText().toString().isEmpty()) {
-                    if (toggle) {
-                        b5.setText("O");
-                        toggle = false;
-                        tv.setText("Joueur X, à vous de jouer !");
-                        cl.setBackgroundColor(Color.rgb(224, 134, 124));
-                    } else {
-                        b5.setText("X");
-                        toggle = true;
-                        tv.setText("Joueur O, à vous de jouer !");
-                        cl.setBackgroundColor(Color.rgb(118, 152, 231));
-                    }
-                }
-            }
-        });
-        b6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (b6.getText().toString().isEmpty()) {
-                    if (toggle) {
-                        b6.setText("O");
-                        toggle = false;
-                        tv.setText("Joueur X, à vous de jouer !");
-                        cl.setBackgroundColor(Color.rgb(224, 134, 124));
-                    } else {
-                        b6.setText("X");
-                        toggle = true;
-                        tv.setText("Joueur O, à vous de jouer !");
-                        cl.setBackgroundColor(Color.rgb(118, 152, 231));
-                    }
-                }
-            }
-        });
-        b7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (b7.getText().toString().isEmpty()) {
-                    if (toggle) {
-                        b7.setText("O");
-                        toggle = false;
-                        tv.setText("Joueur X, à vous de jouer !");
-                        cl.setBackgroundColor(Color.rgb(224, 134, 124));
-                    } else {
-                        b7.setText("X");
-                        toggle = true;
-                        tv.setText("Joueur O, à vous de jouer !");
-                        cl.setBackgroundColor(Color.rgb(118, 152, 231));
-                    }
-                }
-            }
-        });
-        b8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (b8.getText().toString().isEmpty()) {
-                    if (toggle) {
-                        b8.setText("O");
-                        toggle = false;
-                        tv.setText("Joueur X, à vous de jouer !");
-                        cl.setBackgroundColor(Color.rgb(224, 134, 124));
-                    } else {
-                        b8.setText("X");
-                        toggle = true;
-                        tv.setText("Joueur O, à vous de jouer !");
-                        cl.setBackgroundColor(Color.rgb(118, 152, 231));
-                    }
-                }
-            }
-        });
-        b9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (b9.getText().toString().isEmpty()) {
-                    if (toggle) {
-                        b9.setText("O");
-                        toggle = false;
-                        tv.setText("Joueur X, à vous de jouer !");
-                        cl.setBackgroundColor(Color.rgb(224, 134, 124));
-                    } else {
-                        b9.setText("X");
-                        toggle = true;
-                        tv.setText("Joueur O, à vous de jouer !");
-                        cl.setBackgroundColor(Color.rgb(118, 152, 231));
-
-                    }
-                }
-
-            }
-        });
     }
 
-    public boolean verif_win(String player) {
-        if (! player.isEmpty()) {
-            //1ere ligne
-            if ((b1.getText().toString() == b2.getText().toString()) && (b1.getText().toString() == b3.getText().toString()))
-                return true;
-            //2eme ligne
-            if ((b4.getText().toString() == b5.getText().toString()) && (b4.getText().toString() == b6.getText().toString()))
-                return true;
-            //3eme ligne
-            if ((b7.getText().toString() == b8.getText().toString()) && (b7.getText().toString() == b9.getText().toString()))
-                return true;
-            //1ere colonne
-            if ((b1.getText().toString() == b4.getText().toString()) && (b1.getText().toString() == b7.getText().toString()))
-                return true;
-            //2eme colonne
-            if ((b2.getText().toString() == b5.getText().toString()) && (b2.getText().toString() == b8.getText().toString()))
-                return true;
-            //3eme colonne
-            if ((b3.getText().toString() == b6.getText().toString()) && (b3.getText().toString() == b9.getText().toString()))
-                return true;
-            //1ere diagonale
-            if ((b1.getText().toString() == b5.getText().toString()) && (b1.getText().toString() == b9.getText().toString()))
-                return true;
-            //1ere diagonale
-            if ((b3.getText().toString() == b5.getText().toString()) && (b3.getText().toString() == b7.getText().toString()))
-                return true;
+    public void onClickB(View v) {
+        if (((Button) v).getText().toString().isEmpty()) {
+            if (toggle) {
+                ((Button) v).setText("O");
+                toggle = false;
+                tv.setText("Joueur X, à vous de jouer !");
+                cl.setBackgroundColor(Color.rgb(224, 134, 124));
+
+            } else {
+                ((Button) v).setText("X");
+                toggle = true;
+                tv.setText("Joueur O, à vous de jouer !");
+                cl.setBackgroundColor(Color.rgb(118, 152, 231));
+            }
         }
+
+        if (verif_win_O()){
+            tv.setText("Joueur O, vous avez gagné !");
+            cl.setBackgroundColor(Color.rgb(118, 152, 231));
+        } else if (verif_win_X()) {
+            tv.setText("Joueur X, vous avez gagné !");
+            cl.setBackgroundColor(Color.rgb(224, 134, 124));
+        }
+
+    }
+
+    public boolean verif_win_O() {
+
+        //1ere ligne
+        if ((b1.getText() == "O") && (b1.getText() == b2.getText()) && (b1.getText() == b3.getText()))
+            return true;
+        //2eme ligne
+        if ((b4.getText() == "O") && (b4.getText() == b5.getText()) && (b4.getText() == b6.getText()))
+            return true;
+        //3eme ligne
+        if ((b7.getText() == "O") && (b7.getText() == b8.getText()) && (b7.getText() == b9.getText()))
+            return true;
+        //1ere colonne
+        if ((b1.getText() == "O") && (b1.getText() == b4.getText()) && (b1.getText() == b7.getText()))
+            return true;
+        //2eme colonne
+        if ((b2.getText() == "O") && (b2.getText() == b5.getText()) && (b2.getText() == b8.getText()))
+            return true;
+        //3eme colonne
+        if ((b3.getText() == "O") && (b3.getText() == b6.getText()) && (b3.getText() == b9.getText()))
+            return true;
+        //1ere diagonale
+        if ((b1.getText() == "O") && (b1.getText() == b5.getText()) && (b1.getText() == b9.getText()))
+            return true;
+        //2eme diagonale
+        if ((b3.getText() == "O") && (b3.getText() == b5.getText()) && (b3.getText() == b7.getText()))
+            return true;
+
+        return false;
+    }
+
+    public boolean verif_win_X() {
+
+        //1ere ligne
+        if ((b1.getText() == "X") && (b1.getText() == b2.getText()) && (b1.getText() == b3.getText()))
+            return true;
+        //2eme ligne
+        if ((b4.getText() == "X") && (b4.getText() == b5.getText()) && (b4.getText() == b6.getText()))
+            return true;
+        //3eme ligne
+        if ((b7.getText() == "X") && (b7.getText() == b8.getText()) && (b7.getText() == b9.getText()))
+            return true;
+        //1ere colonne
+        if ((b1.getText() == "X") && (b1.getText() == b4.getText()) && (b1.getText() == b7.getText()))
+            return true;
+        //2eme colonne
+        if ((b2.getText() == "X") && (b2.getText() == b5.getText()) && (b2.getText() == b8.getText()))
+            return true;
+        //3eme colonne
+        if ((b3.getText() == "X") && (b3.getText() == b6.getText()) && (b3.getText() == b9.getText()))
+            return true;
+        //1ere diagonale
+        if ((b1.getText() == "X") && (b1.getText() == b5.getText()) && (b1.getText() == b9.getText()))
+            return true;
+        //2eme diagonale
+        if ((b3.getText() == "X") && (b3.getText() == b5.getText()) && (b3.getText() == b7.getText()))
+            return true;
+
         return false;
     }
 }
